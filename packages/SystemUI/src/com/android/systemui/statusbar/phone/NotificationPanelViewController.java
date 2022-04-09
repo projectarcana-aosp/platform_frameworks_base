237f6c1caacc0abd0eb50a8eeb2f2156d2756e9b
dlwlrma123 <alexfinhart@gmail.com>
2022-04-09 18:57:23 +0800

    Revert "Quick Settings: Redesign"
    This reverts commit 7ad1f1f7ef134cc5b56635550661e2251b6ae3d1.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4561,9 +4561,6 @@ public class NotificationPanelViewController extends PanelViewController {
                 mView.setAccessibilityPaneTitle(determineAccessibilityPaneTitle());
             }
             mNotificationStackScrollLayoutController.setMaxTopPadding(mQsMaxExpansionHeight);
-            float qsExpansionFraction = computeQsExpansionFraction();
-            int qsPanelBottomY = calculateQsBottomPosition(qsExpansionFraction);
-            mScrimController.setQsPosition(qsExpansionFraction, qsPanelBottomY);
         }
     }
 
